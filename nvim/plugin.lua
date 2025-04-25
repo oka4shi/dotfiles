f816c1f3ce33bb7cf63fe5d06dc617700d4ca35d
OKAMOTO Shigehiro <primary@oka4shi.dev>
2025-04-25 17:03:03 +0900

    [nvim] Use Buildin LSP

--- nvim/plugin.lua
+++ nvim/plugin.lua
@@ -214,8 +214,6 @@ require('jetpack.packer').startup(function(use)
     use 'williamboman/mason.nvim'
     use 'williamboman/mason-lspconfig.nvim'
 
-    use 'j-hui/fidget.nvim'
-
     use "hrsh7th/nvim-cmp"
     use "hrsh7th/cmp-nvim-lsp"
     use "hrsh7th/vim-vsnip"
@@ -227,35 +225,31 @@ end)
 -- 1. LSP Sever management
 require('mason').setup()
 require('mason-lspconfig').setup_handlers({ function(server)
-    local opt = {
-        -- Function executed when the LSP server startup
-        capabilities = require('cmp_nvim_lsp').default_capabilities(
-            vim.lsp.protocol.make_client_capabilities()
-        )
-    }
-    require('lspconfig')[server].setup(opt)
-    require('lspconfig').astro.setup{}
+    vim.lsp.enable(server)
 end })
 
 -- 2. build-in LSP function
 -- keyboard shortcut
-vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', '<Leader>b', '<cmd>lua vim.lsp.buf.format()<CR>')
-vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+vim.keymap.set('n', 'K',  vim.lsp.buf.hover, keyopts)
+vim.keymap.set('n', '<Leader>b', vim.lsp.buf.format, keyopts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, keyopts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keyopts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, keyopts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keyopts)
+vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, keyopts)
+vim.keymap.set('n', 'gn', vim.lsp.buf.rename, keyopts)
+vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, keyopts)
+vim.keymap.set('n', 'ge', vim.diagnostic.open_float, keyopts)
+vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, keyopts)
+vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev, keyopts)
+
+
 
 -- LSP handlers
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {}
 )
+
 
 -- Reference highlight
 vim.cmd [[
@@ -264,16 +258,25 @@ highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline gu
 highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
 highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
 ]]
-vim.api.nvim_create_augroup('lsp_document_highlight', {})
-vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
-    group = 'lsp_document_highlight',
-    callback = function () vim.lsp.buf.document_highlight() end
 
-})
-vim.api.nvim_create_autocmd({'CursorMoved', 'CursorMovedI'}, {
-    group = 'lsp_document_highlight',
-    callback = function () vim.lsp.buf.clear_references() end
-
+vim.api.nvim_create_autocmd('LspAttach', {
+    -- Reference: https://blog.devoc.ninja/2025/nvim-v0-11-0-language-server-feature/
+    callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if client:supports_method('textDocument/documentHighlight') then
+        vim.api.nvim_create_augroup('lsp_document_highlight', {})
+        vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
+            group = 'lsp_document_highlight',
+                callback = function ()
+                vim.lsp.buf.document_highlight()
+                end
+        })
+        vim.api.nvim_create_autocmd({'CursorMoved', 'CursorMovedI'}, {
+            group = 'lsp_document_highlight',
+            callback = function () vim.lsp.buf.clear_references() end
+        })
+        end
+    end
 })
 
 -- 3. completion (hrsh7th/nvim-cmp)
